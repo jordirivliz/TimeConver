5a7d7c5ed72635a4b8168b87b2dd417128449832
Jorge Rivera <j9rivera@ucsd.edu>
2021-06-30 21:01:39 -0700

    Add files via upload

---
+++ TimeConverApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverApp.swift
+//  TimeConver
+//
+//  Created by Jordi Rivera Lizarralde on 30/6/21.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
